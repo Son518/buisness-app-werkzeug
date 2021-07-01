e87370a7020a9c63a8d5300bc4dc7a39417d1c6a
Son518 <ujin518@outlook.com>
2021-07-01 17:05:14 +0100

    ddd

--- shorty/views.py
+++ shorty/views.py
@@ -94,8 +94,9 @@ def index(request):
     business_news = session.query(News).filter(News.news_category=='Business').order_by(desc(News.news_created)).limit(1).first()
     technology_news = session.query(News).filter(News.news_category=='Technology').order_by(desc(News.news_created)).limit(1).first()
     print("news",latest_news, business_news, politics_news, markets_news, technology_news)
+    return render_template("index.html", usersession=usersession, latest_news=latest_news, markets_news=markets_news,\
+        politics_news=politics_news, business_news=business_news, technology_news=technology_news)
     
-    return render_template("index.html", usersession=usersession, latest_news=latest_news, markets_news=markets_news, politics_news=politics_news, business_news=business_news, technology_news=technology_news)
 @expose("/signin")
 def signin(request):
     login_err_msg=""
